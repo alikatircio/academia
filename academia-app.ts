0021653867088e2fbd204b7c638143f77f260323
Ali Katırcıo <ali.katircio@gmail.com>
2025-07-12 21:00:05 +0300

    added email validate control

--- academia-app.ts
+++ academia-app.ts
@@ -1,46 +1,34 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { SchoolType } from "@prisma/client";
 import e, { Request, Response } from "express";
+import { Helper } from "./api-helper";
 
 const app = express();
 const prisma = new PrismaClient();
+const helper = new Helper();
 app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
 
-async function verifyExistUser(req: any): Promise<{ error: string } | null> {
-  let { email, username } = req.body;
-  const existingEmail = await prisma.user.findFirst({
-    where: {
-      email: email,
-    },
-  });
-  const existingUsername = await prisma.user.findFirst({
-    where: {
-      username: username,
-    },
-  });
-  if (existingEmail) {
-    return { error: "email adresi kullanılmaktadır." };
-  }
-  if (existingUsername) {
-    return { error: "username kullanılmaktadır." };
-  }
-  return null;
-}
-
 app.post("/user", async (req: any, res: any) => {
   const email: string = req.body.email;
   const username: string = req.body.username;
   const password: string = req.body.password;
 
-  const existUser = await verifyExistUser(req);
+  const validateEmail = await helper.validateEmail(email);
+  console.log(validateEmail);
+  if (validateEmail) {
+    return res
+      .status(400)
+      .json({ error: "Kayıt başarısız", details: validateEmail });
+  }
+  const existUser = await helper.verifyExistUser(req, prisma);
   if (existUser) {
-    return res.status(400).json({ error: "Kayıt başarısız", details: existUser });
-    
+    return res
+      .status(400)
+      .json({ error: "Kayıt başarısız", details: existUser });
   }
 
   try {
@@ -53,37 +41,10 @@ app.post("/user", async (req: any, res: any) => {
   }
 });
 
-async function verifySchoolType(req: any): Promise<{ error: string } | null> {
-  let { type, department, userId } = req.body;
-
-  if (!Object.values(SchoolType).includes(type)) {
-    return { error: "Geçersiz eğitim tipi!" };
-  }
-
-  if (type !== SchoolType.UNI && department) {
-    return { error: "department bilgisi sadece UNI için gönderilebilir." };
-  }
-
-  if (type === SchoolType.UNI && !department) {
-    return { error: "UNI tipi eğitimde department alanı zorunludur." };
-  }
-
-  const existingEducation = await prisma.education.findFirst({
-    where: {
-      userId: Number(userId),
-      type,
-    },
-  });
-  if (existingEducation) {
-    return { error: `Bu kullanıcıya ait zaten bir ${type} eğitimi mevcut!` };
-  }
-  return null;
-}
-
 app.post("/education", async (req: any, res: any) => {
   let { type, city, department, year, userId } = req.body;
 
-  const error = await verifySchoolType(req);
+  const error = await helper.verifySchoolType(req, prisma);
   if (error) {
     return res.status(400).json(error);
   }
